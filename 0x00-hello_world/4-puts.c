10b3c8a39fcb2d59b71c81fdac597e6c7c89bd2b
sami-hamid <samifadlallah21@gmail.com>
2023-09-08 10:12:12 -0700

    my alx_low_programming hellow,world project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry point
@@ -8,7 +7,6 @@
 int main(void)
 	
 {
-	puts("Programming is like building a multilingual puzzle"\");
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
 * main - Entry point
@@ -14,4 +13,3 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
